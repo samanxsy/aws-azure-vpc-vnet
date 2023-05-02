9cbc77906cc12d1b0fdbb944cd229ae9eeadca79
samanxsy <samanxsy@gmail.com>
2023-05-02 23:38:44 +0200

    s3 region changed

--- AWS/main.tf
+++ AWS/main.tf
@@ -6,7 +6,7 @@ terraform {
     backend "s3" {
         bucket = "terraform-state-bucket"
         key = "terraform.tfstate"
-        region = "eu-central-1"
+        region = "us-east-1"
         encrypt = true
         dynamodb_table = "terraform-state-lock"
     }
