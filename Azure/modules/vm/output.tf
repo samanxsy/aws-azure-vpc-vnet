56d0dc3d3879b0a7d99a4e1064d31a59fbca2542
samanxsy <samanxsy@gmail.com>
2023-05-05 16:40:16 +0200

    Azure infrastructure separated

--- Azure/modules/vm/output.tf
+++
@@ -1,7 +0,0 @@
-output "vm_name" {
-  value = azurerm_virtual_machine.xvm.name
-}
-
-output "vm_public_ip" {
-  value = azurerm_virtual_machine.xvm.vm_public_ip
-}
--- Azure/output.tf
+++
@@ -1,4 +0,0 @@
-output "storage-access-key1" {
-    value = azurerm_storage_account.storage.primary_access_key
-    sensitive = true
-}
